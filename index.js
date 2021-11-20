f575de7446e9ed0219de04771f3932d57d74331b
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-20 23:17:57 +0530

    Adding code for flattening an n-dimensional array

--- index.js
+++ index.js
@@ -477,4 +477,17 @@ Promise.myAll(promiseArray).then(function(response){
     console.log(response);
 });
 
+//Code for flattening an array
+
+const flatten = function(arr, result = []) {
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i];
+    if (Array.isArray(value)) {
+      flatten(value, result);
+    } else {
+      result.push(value);
+    }
+  }
+  return result;
+};
 
